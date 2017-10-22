242f913f5613f366e3140d01ff335c3c5ae29c3a
Lewis <lewisyoul@gmail.com>
2017-10-22 16:41:03 +0100

    Adds lib dir containing roman_numerals.rb

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,24 @@
+class Integer
+  ROMAN_NUMERAL = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
+
+
+  def convert_to_numeral
+    num = self
+    string = ""
+    ROMAN_NUMERAL.each do |k, v|
+      (num/k).times { string << v ; num -= k }
+    end
+    string
+  end
+
+end
